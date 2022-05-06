3c9cfeafa7dcac3729c2ced727aa5aa050882608
vietnq2291 <viet.nq194881@sis.hust.edu.vn>
2022-05-06 12:38:13 +0700

    Finish all lab04

--- lesson4/merge.c
+++ lesson4/merge.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
-#define MAX_LEN 80
+enum {SUCCESS, FAIL, MAX_LEN = 80};
 
 void merge_file(FILE *fin1, FILE *fin2, FILE *fout){
-    char buff1[MAX_LEN],
-         buff2[MAX_LEN];
-    int len;
+    char buff1[MAX_LEN + 1],
+         buff2[MAX_LEN + 1];
 
-    while ((fgets(buff1, MAX_LEN, fin1) != NULL) && (fgets(buff2, MAX_LEN, fin2) != NULL)){
+    // Read from 2 file and write simutaneously
+    while (fgets(buff1, MAX_LEN, fin1) != NULL && fgets(buff2, MAX_LEN, fin2) != NULL){
         fputs(buff1, fout);
         fputs(buff2, fout);
     }
-    if (buff1 != NULL) fputs(buff1, fout);
 
+    // print last read buffer from fin1 if fin2 ends earlier
+    if (buff1 != NULL){
+        fputs(buff1, fout);
+    }
+
+    // Read from 1 file only and write
     while (fgets(buff1, MAX_LEN, fin1) != NULL){
         fputs(buff1, fout);
     }
@@ -21,26 +26,25 @@ void merge_file(FILE *fin1, FILE *fin2, FILE *fout){
 }
 
 int main(int argc, char* argv[]){
+    int reval = SUCCESS;
     FILE *fin1, *fin2, *fout;
 
+    // check number of arguments
     if (argc != 4){
-        printf("The correct syntax is: merge file1 file2 file3\n");
-        return 1;
-    }
-
-    // Read file
-    if ((fin1 = fopen(argv[1], "r")) == NULL){
-        printf("Cannot open file %s\n", argv[1]);
-        return 1;
+        printf("Wrong number of arguments!\n");
+        printf("The correct syntax is: merge <file1> <file2> <file3>\n");
+    } else if ((fin1 = fopen(argv[1], "r")) == NULL){
+        printf("Cannot open %s.\n", argv[1]);
+        reval = FAIL;
     } else if ((fin2 = fopen(argv[2], "r")) == NULL){
-        printf("Cannot open file %s\n", argv[2]);
-        return 1;
+        printf("Cannot open %s.\n", argv[2]);
+        reval = FAIL;
     } else if ((fout = fopen(argv[3], "w")) == NULL){
-        printf("Cannot open file %s\n", argv[3]);
-        return 1;
+        printf("Cannot open %s.\n", argv[3]);
+        reval = FAIL;
+    } else {
+        merge_file(fin1, fin2, fout);
     }
 
-    merge_file(fin1, fin2, fout);
-
-    return 0;
+    return reval;
 }
